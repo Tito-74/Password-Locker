ea81cebad6678b9ec520de97506398744ef17703
Tito-74 <kipkirui133@gmail.com>
2021-09-05 12:24:48 +0300

    adding User class methods

--- run.py
+++ run.py
@@ -4,8 +4,30 @@ import string
 from password_locker import User
 from password_locker import Credentials
 
+#user Functions
+def add_user(username,userPassword):
+    '''
+    function to add new user 
+    '''
+    new_user= User(username,userPassword)
+    return new_user
+def save_new_user(user):
+    '''
+    Function to save credential
+    '''
+    user.save_user()
+def find_user(username,userPassword):
+    '''
+    Function that finds a credential by number and returns the credential
+    '''
+    return User.find_user(username,userPassword)
+def check_existing_user(username,userPassword):
+    '''
+    Function that check if a user exists with that number and return a Boolean
+    '''
+    return User.user_exist(username,userPassword)
 
-
+# Credential Functions
 
 
 if __name__ == '__main__':
