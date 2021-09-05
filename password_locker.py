6cc3da494f64ac6fa0b51538e02b5a9eb2f849f7
Tito-74 <kipkirui133@gmail.com>
2021-09-05 12:04:54 +0300

    adding method that check user existance

--- password_locker.py
+++ password_locker.py
@@ -10,7 +10,22 @@ class User:
         Function to save user
         '''
         User.user_list.append(self)
+    @classmethod
+    def user_exist(cls,username,userPassword):
+        # '''
+        # Method that checks if a credential exists from the credential list.
+        # Args:
+        #     number: Phone number to search if it exists
+        # Returns :
+        #     Boolean: True or false depending if the credential exists
+        # '''
+        for user in cls.user_list:
+            if user.username == username and user.userPassword == userPassword:
+                    return True
+
+        return False
     
+
 class Credentials(User):
     credential_list = []
     def __init__(self,username,userPassword,accountName):
